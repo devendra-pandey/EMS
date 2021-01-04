cc57f6579b36fe12a3de0d0082b76272aa5346e1
devendra-pandey <devendra.dpandey02@gmail.com>
2021-01-04 10:31:07 +0530

    user validation done

--- user/views.py
+++ user/views.py
@@ -2,19 +2,42 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth# Create your views here.
 from django.contrib.auth import logout
+from django.contrib import messages
+
+# def login(request):
+#     if request.method =="POST":
+#         ##che  if the user exist withthe pass
+#         uname = request.POST['username']
+#         pwd = request.POST['password']
+#         user = auth.authenticate(username=uname,password=pwd)
+#         if user.is_admin is not None:
+#             auth.login(request, user)
+#             return redirect("/employee_admin")
+#         else:
+#             return render(request, 'user/login.html', {'error':"Invalid Login Credentials"})
+#     else:
+#         return render(request, 'user/login.html')
+
+
 def login(request):
-    if request.method =="POST":
-        ##che  if the user exist withthe pass
-        uname = request.POST['username']
-        pwd = request.POST['password']
-        user = auth.authenticate(username=uname,password=pwd)
-        if user.is_admin is not None:
-            auth.login(request, user)
+    if request.method == 'POST':
+
+        #AuthenticationForm_can_also_be_used__
+
+        uname = request.POST.get('username')
+        pwd = request.POST.get('password')
+        user = auth.authenticate(request, username=uname, password=pwd)
+        if user is not None:
+            auth.login(request,user)
             return redirect("/employee_admin")
         else:
-            return render(request, 'user/login.html', {'error':"Invalid Login Credentials"})
-    else:
-        return render(request, 'user/login.html')
+            messages.error(request,'username or password not correct')
+            return redirect('login')
+
+    return render(request, 'user/login.html')
+
+
+
 
 
 def logout_request(request):
